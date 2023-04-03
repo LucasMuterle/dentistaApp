0a8b68d186922d839f31756c629e15ded1558d42
Raul Migliari <raulmigliari5@gmail.com>
2023-04-03 18:22:00 -0300

    Intent da tela principal para de cadastro

--- app/src/main/java/com/example/dentistaapp/MainActivity.kt
+++ app/src/main/java/com/example/dentistaapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.dentistaapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.cadastro)
+        setContentView(R.layout.activity_main)
+
+        val buttonCadastro = findViewById<AppCompatButton>(R.id.btnCriarCad)
+
+        buttonCadastro.setOnClickListener {
+
+            val intent = Intent(this,FormCadastro::class.java)
+            startActivity(intent)
+
+        }
     }
 }
